dc85c66b61b18a974d1a5f6dfea5491628366a83
Gerardo Gutierrez <ggutierrezdieck@gmail.com>
2020-10-21 10:34:43 -0500

    Changed settings

--- survey/settings.py
+++ survey/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
         'DIRS': [
                 os.path.join(BASE_DIR, "pacientes", "templates"),
                 os.path.join(BASE_DIR, "userauth", "templates"),
+                os.path.join(BASE_DIR, "templates"),
                 ],
         'APP_DIRS': True,
         'OPTIONS': {
